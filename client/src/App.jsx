69d7095e065fd83044e6142148da66380f0a99a5
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2025-04-26 00:29:00 +0530

    🔰Frontend setup and ShadcnUI setup DONE

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-center p-16">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nostrum
+        nulla. Quasi sint inventore vel optio obcaecati dicta voluptas
+        architecto.
+      </h1>
+      <Button variant="default">Button</Button>
+    </>
+  );
+}
+
+export default App
